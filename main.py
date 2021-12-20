c0c6f31f7e5750b30251795af6d658db056176d7
ffedor <ffedor.ffedorov2005@gmail.com>
2021-12-20 22:24:16 +0300

    added game class, changed heroes spawn method (w bugs)

--- main.py
+++ main.py
@@ -11,38 +11,6 @@ COLOR_NOT_WANTED = ""
 COLOR_WANTED = ""
 
 
-def load_image(name, name_directory, colorkey=None):
-    fullname = os.path.join(name_directory, name)
-    # если файл не существует, то выходим
-    if not os.path.isfile(fullname):
-        print(f"Файл с изображением '{fullname}' не найден")
-        sys.exit()
-    image = pygame.image.load(fullname)
-    return image
-
-
-def update_level(heroes_count):
-    wanted = True
-    table_info = True
-    Hero(heroes_sprites_group, wanted, table_info)
-    for i in range(heroes_count):
-        Hero(heroes_sprites_group, wanted)
-        wanted = False
-
-
-def choose_color(name_directory, *colors_used):
-    colors = COLORS[:]
-    if colors_used:
-        for color in colors_used:
-            if color in colors:
-                colors.remove(color)
-    color_not_wanted = colors[rnd.randrange(len(colors))]
-    colors.remove(color_not_wanted)
-    color_wanted = colors[rnd.randrange(len(colors))]
-    return (color_not_wanted, color_wanted)
-
-
-
 class Board:
     # создание поля
     def __init__(self, width, height):
@@ -95,41 +63,169 @@ class Board:
         return self.on_click(cell)
 
 
-class Hero(pygame.sprite.Sprite):
-    color_hero_not_wanted, color_hero_wanted = choose_color("data\smiles_1")
-    image_not_wanted = load_image(color_hero_not_wanted, "data\smiles_1")
-    image_wanted = load_image(color_hero_wanted, "data\smiles_1")
+class Game:
 
-    def __init__(self, group, wanted, table_info=False):
+    def __init__(self, lvl):
+        pygame.init()
+        pygame.display.set_caption('игра')
+
+        self.size = SCREEN_SIZE
+        self.fps = FPS
+        self.aim_size = AIM_SIZE
+        self.aim_size_x = AIM_SIZE[0]
+        self.aim_size_y = AIM_SIZE[1]
+        self.aim_size_x_half = AIM_SIZE_HALF[0]
+        self.aim_size_y_half = AIM_SIZE_HALF[1]
+
+        self.screen = pygame.display.set_mode(self.size)
+        self.screen2 = pygame.display.set_mode(self.size)
+        self.screen3 = pygame.display.set_mode(self.size)
+        self.screen4 = pygame.display.set_mode(self.size)
+
+        self.clock = pygame.time.Clock()
+
+        self.aim_sprites_group = pygame.sprite.Group()
+        self.aim_sprite = pygame.sprite.Sprite()  # создадим спрайт
+        self.aim_sprite.image = self.load_image("aim1.png", "data")  # определим его вид
+        self.aim_sprite.rect = self.aim_sprite.image.get_rect()  # и размеры
+        self.aim_sprites_group.add(self.aim_sprite)  # добавим спрайт в группу
+
+        self.heroes_sprites_group = pygame.sprite.Group()
+
+        self.x, self.y = 10, 10
+        self.lvl = lvl
+
+        self.update_level(self.lvl)
+
+        self.running = True
+
+    def run(self):
+        while self.running:
+            self.screen.fill('white')
+            # board.render(screen)
+            self.heroes_sprites_group.draw(self.screen3)
+
+            for event in pygame.event.get():
+
+                self.heroes_sprites_group.update(event)  # обновление позиции персонажей
+
+                if event.type == pygame.QUIT:
+                    self.running = False
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    # print(board.get_click(event.pos))  # вывод координат клеткиad
+                    pass
+                if event.type == pygame.MOUSEMOTION:
+                    self.x, self.y = event.pos
+
+            if pygame.mouse.get_focused():
+                pygame.mouse.set_visible(False)
+
+                self.aim_sprite.rect.x = self.x - self.aim_size_x_half  # 25 - половина размера прицела
+                self.aim_sprite.rect.y = self.y - self.aim_size_y_half  # 25 - половина размера прицела
+                self.aim_sprites_group.draw(self.screen2)
+
+            for hero in self.heroes_sprites_group:
+                hero.move()
+
+            self.clock.tick(self.fps)
+            pygame.display.flip()
+
+        pygame.quit()
+
+    def load_image(self, name, name_directory, colorkey=None):
+        fullname = os.path.join(name_directory, name)
+        # если файл не существует, то выходим
+        if not os.path.isfile(fullname):
+            print(f"Файл с изображением '{fullname}' не найден")
+            sys.exit()
+        image = pygame.image.load(fullname)
+        return image
+
+    def update_level(self, heroes_count):
+        wanted = True
+        table_info = True
+        print(10)
+        Hero(self.heroes_sprites_group, wanted, self.lvl, table_info)
+        for i in range(heroes_count):
+            Hero(self.heroes_sprites_group, self.lvl, wanted)
+            wanted = False
+
+    def choose_color(self, name_directory, *colors_used):
+        colors = COLORS[:]
+        if colors_used:
+            for color in colors_used:
+                if color in colors:
+                    colors.remove(color)
+        color_not_wanted = colors[rnd.randrange(len(colors))]
+        colors.remove(color_not_wanted)
+        color_wanted = colors[rnd.randrange(len(colors))]
+        return (color_not_wanted, color_wanted)
+
+
+class Hero(pygame.sprite.Sprite):
+    # color_hero_not_wanted, color_hero_wanted = Game.choose_color("data\smiles_1")
+    # image_not_wanted = Game.load_image(color_hero_not_wanted, "data\smiles_1")
+    # image_wanted = Game.load_image(color_hero_wanted, "data\smiles_1")
+
+    def __init__(self, heroes_sprites_group, wanted, lvl, table_info=False):
         # НЕОБХОДИМО вызвать конструктор родительского класса Sprite.
         # Это очень важно !!!
-        super().__init__(group)
-        # image_wanted, image_not_wanted, color_hero_wanted, color_hero_not_wanted = self.change_color()
-        # self.color_hero_wanted, self.color_hero_not_wanted = color_hero_wanted, color_hero_not_wanted
+        super().__init__(heroes_sprites_group)
+
+        image_wanted, image_not_wanted, color_hero_wanted, color_hero_not_wanted = self.change_color()
+        self.color_hero_wanted, self.color_hero_not_wanted = color_hero_wanted, color_hero_not_wanted
+
+        self.heroes_sprites_group = heroes_sprites_group
+
         self.score = 0
+        self.lvl = lvl
+
         self.table_info = table_info
+
         if wanted:
-            self.image = Hero.image_wanted
+            self.image = image_wanted
             self.update_info_board(self.score)
         else:
-            self.image = Hero.image_not_wanted
+            self.image = image_not_wanted
+
         self.wanted = wanted
+
         self.rect = self.image.get_rect()
+
         self.v = V // FPS
+
         self.update_position()
+
         self.create_v()
 
     def change_color(self, *colors_used):
-        color_hero_not_wanted, color_hero_wanted = choose_color("data\smiles_1")
-        image_not_wanted = load_image(color_hero_not_wanted, "data\smiles_1")
-        image_wanted = load_image(color_hero_wanted, "data\smiles_1")
+        color_hero_not_wanted, color_hero_wanted = self.choose_color("data\smiles_1")
+        image_not_wanted = self.load_image(color_hero_not_wanted, "data\smiles_1")
+        image_wanted = self.load_image(color_hero_wanted, "data\smiles_1")
         return (image_wanted, image_not_wanted, color_hero_wanted, color_hero_not_wanted)
 
     def create_colors(self):
         pass
 
+    def change_lvl(self):
+        # менять уровень в зависимости от кол - ва очков
+        # чем больше уровень, тем выше скорость движения, больше человечков
+        pass
+
+    def choose_color(self, name_directory, *colors_used):
+        colors = COLORS[:]
+        if colors_used:
+            for color in colors_used:
+                if color in colors:
+                    colors.remove(color)
+        color_not_wanted = colors[rnd.randrange(len(colors))]
+        colors.remove(color_not_wanted)
+        color_wanted = colors[rnd.randrange(len(colors))]
+        return (color_not_wanted, color_wanted)
+
     def update_info_board(self, score):
-        InfoBoard(screen4, Hero.image_wanted, score)
+        pass
+        # InfoBoard(screen4, image_wanted, score)
 
     def update_position(self):
         if self.table_info:
@@ -137,6 +233,15 @@ class Hero(pygame.sprite.Sprite):
         else:
             self.rect.x = rnd.randrange(70, GAME_FIELD_WIDTH)
             self.rect.y = rnd.randrange(70, GAME_FILED_HEIGHT)
+
+    def load_image(self, name, name_directory, colorkey=None):
+        fullname = os.path.join(name_directory, name)
+        # если файл не существует, то выходим
+        if not os.path.isfile(fullname):
+            print(f"Файл с изображением '{fullname}' не найден")
+            sys.exit()
+        image = pygame.image.load(fullname)
+        return image
 
     def update(self, *args):
         if self.wanted and not self.table_info:
@@ -146,10 +251,13 @@ class Hero(pygame.sprite.Sprite):
                 #     # hero.change_v(2)
                 #     #hero.change_color()
                 #     hero.jump()
-                for hero in heroes_sprites_group:
+                for hero in self.heroes_sprites_group:
                     # hero.change_v(2)
                     #hero.change_color()
-                    hero.update_position()
+                    self.heroes_sprites_group.remove(hero)
+                    # hero.update_position()
+                self.lvl += 1
+                Game(self.lvl).run()
                 self.score += 1
                 self.update_info_board(self.score)
                 print(self.score)
@@ -222,70 +330,67 @@ class InfoBoard:
 
 
 if __name__ == '__main__':
-    pygame.init()
-    pygame.display.set_caption('игра')
-    board = Board(30, 20)
-
-    size = SCREEN_SIZE
-    fps = FPS
-    aim_size = AIM_SIZE
-    aim_size_x = AIM_SIZE[0]
-    aim_size_y = AIM_SIZE[1]
-    aim_size_x_half = AIM_SIZE_HALF[0]
-    aim_size_y_half = AIM_SIZE_HALF[1]
-
-    screen = pygame.display.set_mode(size)
-    screen2 = pygame.display.set_mode(size)
-    screen3 = pygame.display.set_mode(size)
-    screen4 = pygame.display.set_mode(size)
-
-    game_info = InfoBoard(screen4, Hero.image_wanted, 0)
-
-    clock = pygame.time.Clock()
-
-    # группа прицел
-    aim_sprites_group = pygame.sprite.Group()
-    # создадим спрайт
-    aim_sprite = pygame.sprite.Sprite()
-    # определим его вид
-    aim_sprite.image = load_image("aim1.png", "data")
-    # и размеры
-    aim_sprite.rect = aim_sprite.image.get_rect()
-    # добавим спрайт в группу
-    aim_sprites_group.add(aim_sprite)
-
-    heroes_sprites_group = pygame.sprite.Group()
-
-    update_level(200)
-    running = True
-    while running:
-        screen.fill('white')
-        # board.render(screen)
-        heroes_sprites_group.draw(screen3)
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                print(board.get_click(event.pos))  # вывод координат клеткиad
-                pass
-            if event.type == pygame.MOUSEMOTION:
-                x, y = event.pos
-
-            heroes_sprites_group.update(event)  # обновление позиции персонажей
-
-        if pygame.mouse.get_focused():
-            pygame.mouse.set_visible(False)
-
-            aim_sprite.rect.x = x - AIM_SIZE_HALF[0]  # 25 - половина размера прицела
-            aim_sprite.rect.y = y - AIM_SIZE_HALF[1]  # 25 - половина размера прицела
-            aim_sprites_group.draw(screen2)
-
-        for hero in heroes_sprites_group:
-            hero.move()
-
-        game_info.draw()
-
-        clock.tick(FPS)
-        pygame.display.flip()
-    pygame.quit()
+    # pygame.init()
+    # pygame.display.set_caption('игра')
+    # board = Board(30, 20)
+    #
+    # size = SCREEN_SIZE
+    # fps = FPS
+    # aim_size = AIM_SIZE
+    # aim_size_x = AIM_SIZE[0]
+    # aim_size_y = AIM_SIZE[1]
+    # aim_size_x_half = AIM_SIZE_HALF[0]
+    # aim_size_y_half = AIM_SIZE_HALF[1]
+    #
+    # screen = pygame.display.set_mode(size)
+    # screen2 = pygame.display.set_mode(size)
+    # screen3 = pygame.display.set_mode(size)
+    # screen4 = pygame.display.set_mode(size)
+    #
+    # clock = pygame.time.Clock()
+    #
+    # # группа прицел
+    # aim_sprites_group = pygame.sprite.Group()
+    # # создадим спрайт
+    # aim_sprite = pygame.sprite.Sprite()
+    # # определим его вид
+    # aim_sprite.image = load_image("aim1.png", "data")
+    # # и размеры
+    # aim_sprite.rect = aim_sprite.image.get_rect()
+    # # добавим спрайт в группу
+    # aim_sprites_group.add(aim_sprite)
+    #
+    # heroes_sprites_group = pygame.sprite.Group()
+    #
+    # update_level(200)
+    # running = True
+    # while running:
+    #     screen.fill('white')
+    #     # board.render(screen)
+    #     heroes_sprites_group.draw(screen3)
+    #
+    #     for event in pygame.event.get():
+    #         if event.type == pygame.QUIT:
+    #             running = False
+    #         if event.type == pygame.MOUSEBUTTONDOWN:
+    #             print(board.get_click(event.pos))  # вывод координат клеткиad
+    #             pass
+    #         if event.type == pygame.MOUSEMOTION:
+    #             x, y = event.pos
+    #
+    #         heroes_sprites_group.update(event)  # обновление позиции персонажей
+    #
+    #     if pygame.mouse.get_focused():
+    #         pygame.mouse.set_visible(False)
+    #
+    #         aim_sprite.rect.x = x - AIM_SIZE_HALF[0]  # 25 - половина размера прицела
+    #         aim_sprite.rect.y = y - AIM_SIZE_HALF[1]  # 25 - половина размера прицела
+    #         aim_sprites_group.draw(screen2)
+    #
+    #     for hero in heroes_sprites_group:
+    #         hero.move()
+    #
+    #     clock.tick(FPS)
+    #     pygame.display.flip()
+    # pygame.quit()
+    Game(1).run()
